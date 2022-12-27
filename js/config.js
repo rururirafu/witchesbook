530b901d7f1c4161f8058dfcc15b538f08580330
rururirafu <rururirafu@gmail.com>
2022-12-27 23:03:45 +0900

    add name

--- js/config.js
+++ js/config.js
@@ -5,15 +5,15 @@ async function getDataFromDB() {
   };
   // data for character
   const master = [
-    { id: '5001', rarity: 5 }, // other attributes are ommitted
-    { id: '5002', rarity: 5 },
-    { id: '5003', rarity: 5 },
-    { id: '4001', rarity: 4 },
-    { id: '4002', rarity: 4 },
-    { id: '4003', rarity: 4 },
-    { id: '3001', rarity: 3 },
-    { id: '3002', rarity: 3 },
-    { id: '3003', rarity: 3 },
+    { id: '5001', rarity: 5, name: 'Angelica' }, // other attributes are ommitted
+    { id: '5002', rarity: 5, name: 'Beatrix' },
+    { id: '5003', rarity: 5, name: 'Charlotte' },
+    { id: '4001', rarity: 4, name: 'Dorothy' },
+    { id: '4002', rarity: 4, name: 'Emil' },
+    { id: '4003', rarity: 4, name: 'Frederica' },
+    { id: '3001', rarity: 3, name: 'Gretel' },
+    { id: '3002', rarity: 3, name: 'Hansel' },
+    { id: '3003', rarity: 3, name: 'Izabel' },
   ];
 
   return [info, master];
@@ -26,7 +26,7 @@ export async function getConfig() {
     const ids = master
       .filter(x => x.rarity === rarity)
       .map(x => x.id);
-    config.push({ rarity, prob, ids });
+    config.push({ rarity, prob, ids, name });
   });
   return config;
 }
